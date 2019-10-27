a5d2ebe61bad4259f688323cc5c3452ef1133db0
Henry Lepa Malomalo <hlepa64@gmail.com>
2019-10-27 19:13:24 +0100

    sunday updates

--- app/controllers/accueil_controller.rb
+++ app/controllers/accueil_controller.rb
@@ -9,7 +9,7 @@ class AccueilController < ApplicationController
 	  @politiques = Actualite.where(category: [1]).limit(2).offset(1).order("created_at DESC")
 	  @sports = Actualite.where(category: [2]).limit(2).offset(1).order("created_at DESC")
 	  @envune = Actualite.where(category: [3]).limit(1).order("created_at DESC")
-	  @environnements = Actualite.where(category: [3]).limit(4).offset(1).order("created_at DESC")
+	  @environnements = Actualite.where(category: [3]).limit(2).offset(1).order("created_at DESC")
 	  @sondages = Sondage.all.limit(1).order("created_at DESC")
 	  @sonds = Sondage.all.limit(5).order("created_at DESC")
 	  @petitions = Petition.all.limit(1).order("created_at DESC")
